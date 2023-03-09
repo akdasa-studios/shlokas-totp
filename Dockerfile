37ba5dd3cd2fdde045734c2b1bcb46e4f760086a
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2023-03-09 15:14:19 +0100

    fix: do not reveal secrets

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,5 @@
-FROM node
+FROM node:18-alpine3.16
+WORKDIR /app
+COPY . .
+RUN npm install
+CMD [ "npm", "run", "serve" ]
--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const logger = require('morgan');
+// const logger = require('morgan');
 const cors = require('cors')
 const otp = require("otp")
 
 
 var app = express();
 app.set('port', 8080);
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors())
 const secret = process.env.SECRET
+var inst = new otp({
+    name: "shlokas",
+    keySize: 64,
+    secret: secret
+});
 
+// prevent otp revealing secrets
+// https://github.com/pipobscure/otp/blob/master/lib/otp.ts#L87
+console.error = () => {}
 
 app.post("/validate", (req, res) => {
     const data = req.body
-
-    var inst = new otp({
-        name: "shlokas",
-        keySize: 64,
-        secret: secret
-    });
     const a = inst.totp();
 
     res.json({ status: data.code == a })
