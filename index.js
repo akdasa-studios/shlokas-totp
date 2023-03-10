43e6c1f7cb120f769951bd1422971db4d64e70b3
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2023-03-10 18:02:23 +0100

    feat: remove cors

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-// const logger = require('morgan');
-const cors = require('cors')
+const logger = require('morgan');
 const otp = require("otp")
 
 
 var app = express();
 app.set('port', 8080);
-// app.use(logger('dev'));
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors())
+
 const secret = process.env.TOTP_SECRET
 var inst = new otp({
     name: "shlokas",
@@ -20,11 +19,12 @@ var inst = new otp({
 
 // prevent otp revealing secrets
 // https://github.com/pipobscure/otp/blob/master/lib/otp.ts#L87
-console.error = () => {}
+// console.error = () => {}
 
 app.post("/validate", (req, res) => {
     const data = req.body
     const a = inst.totp();
+    console.log(data.code, a)
 
     res.json({ status: data.code == a })
 })
